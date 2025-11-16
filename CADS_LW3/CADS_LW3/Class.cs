7cd0d17c740cc88ad39cabc66fb7253713b923db
isctel <stepulenko_kirill@mail.ru>
2025-11-16 18:36:09 +0300

    lab3 1.0

---
+++ CADS_LW3/CADS_LW3/Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADS_LW3
+{
+    internal class Class
+    {
+        private string name;
+        private int mark;
+
+        public string Name { get { return name; } set { name = value; } }
+        public int Mark { get { return mark; } set { if (2 <= value && value <= 5) mark = value; } }
+
+        public Class()
+        {
+            name = "Математический анализ";
+            mark = 5;
+        }
+
+        public Class(string name, int mark)
+        {
+            this.Name = name;
+            this.Mark = mark;
+        }
+    }
+}
---
+++ CADS_LW3/CADS_LW3/University.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CADS_LW3
+{
+    internal class University
+    {
+        private string name;
+        private string[] course;
+        private int[] group;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string[] Course { get { return course; } set { course = value; } }
+        public int[] Group { get { return group; } set { group = value; } }
+
+        public University()
+        {
+            name = "КубГУ";
+            course = new string[] { "ФКТиПМ" };
+            group = new int[] { 11, 12, 13, 14, 15, 16 };
+        }
+
+        public University(string name, string[] course)
+        {
+            this.name = name;
+            this.course = course;
+            group = new int[] { 11, 12, 13, 14, 15, 16 };
+        }
+
+        public University(string name, string[] course, int[] group)
+        {
+            this.name = name;
+            this.course = course;
+            this.group = group;
+        }
+    }
+}
