49a610c240fc1096ae36d142711f7141b9bdd036
isctel <stepulenko_kirill@mail.ru>
2026-02-23 23:26:42 +0300

    struct 19 1.0

---
+++ Struct19/Struct19/Node.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct19
+{
+    class Node<T>
+    {
+        private Tuple<T, bool> value;
+        private Node<T> left;
+        private Node<T> right;
+        private Node<T> parent;
+        private bool isRed;
+
+        public Node(T value)
+        {
+            this.value = new Tuple<T, bool>(value, true);
+            left = null;
+            right = null;
+            parent = null;
+            isRed = true;
+        }
+
+        public Tuple<T, bool> Value { get { return value; } set { this.value = value; } }
+        public Node<T> Left { get { return left; } set { left = value; } }
+        public Node<T> Right { get { return right; } set { right = value; } }
+        public Node<T> Parent { get { return parent; } set { parent = value; } }
+        public bool IsRed { get { return isRed; } set { isRed = value; } }
+
+        public Node<T> Uncle()
+        {
+            if (parent == null || parent.parent == null)
+            {
+                return null;
+            }
+
+            if (parent.IsOnLeft())
+            {
+                return parent.parent.right;
+            }
+            else
+            {
+                return parent.parent.left;
+            }
+        }
+
+        public bool IsOnLeft()
+        {
+            return this == parent.left;
+        }
+
+        public Node<T> Sibling()
+        {
+            if (parent == null)
+            {
+                return null;
+            }
+
+            if (IsOnLeft())
+            {
+                return parent.right;
+            }
+            else
+            {
+                return parent.left;
+            }
+        }
+
+        public void MoveDown(Node<T> newParent)
+        {
+            if (parent != null)
+            {
+                if (IsOnLeft())
+                {
+                    parent.left = newParent;
+                }
+                else
+                {
+                    parent.right = newParent;
+                }
+            }
+            newParent.parent = parent;
+            parent = newParent;
+        }
+
+        public bool HasRedChild()
+        {
+            return (left != null && left.isRed) || (right != null && right.isRed);
+        }
+    }
+}
---
+++ Struct19/Struct19/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeSet<int> set = new TreeSet<int>();
+            set.Add(13);
+            set.Add(28);
+            set.Add(773);
+            set.Add(4);
+            set.Add(90);
+            set.Add(3);
+            set.Add(64);
+            set.Add(3);
+            set.Add(7);
+            set.Add(1350);
+            set.Add(53);
+            set.Add(27);
+            set.Add(7);
+            set.Add(85);
+            Console.WriteLine(String.Join(" ", set.ToArray()));
+            Console.WriteLine(set.Ceiling(86));
+            Console.WriteLine(set.Floor(86));
+            set.Remove(13);
+            Console.WriteLine(String.Join(" ", set.ToArray()));
+            Console.WriteLine(set.Contains(1350));
+            Console.WriteLine(set.Contains(1351));
+            Console.WriteLine(set.Contains(773));
+        }
+    }
+}
